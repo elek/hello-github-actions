7e7d2cb7a8b6694e13e93b673641a265a5171c44
Márton Elek <elek@apache.org>
2019-10-14 22:26:49 +0200

    debug

--- .github/actions/label.sh
+++ .github/actions/label.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -x
+cat $GITHUB_EVENT_PATH
 COMMENT=$(jq  -r '.payload.comment.body' "$GITHUB_EVENT_PATH")
 if [[ $COMMENT == /label* ]]; then
     LABEL=$(echo $COMMENT | awk '{print $2}')
