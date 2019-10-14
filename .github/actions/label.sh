2a2050058d7f3ac5b2e7a576f554cff6f07c1c69
Márton Elek <elek@apache.org>
2019-10-14 22:25:37 +0200

    debug

--- .github/actions/label.sh
+++ .github/actions/label.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+set -x
 COMMENT=$(jq  -r '.payload.comment.body' "$GITHUB_EVENT_PATH")
 if [[ $COMMENT == /label* ]]; then
     LABEL=$(echo $COMMENT | awk '{print $2}')
     LABEL_URL=$(jq -r '.payload.issue.labels_url' "$GITHUB_EVENT_PATH" | sed 's/{\/name}//g')
-    curl --header "authorization: Bearer $GITHUB_TOKEN" -X POST --data  "{\"labels\": [\"$LABEL\"]}" $LABEL_URL
+    curl --header "authorization: Bearer GITHUB_TOKEN" -X POST --data  "{\"labels\": [\"$LABEL\"]}" $LABEL_URL
 fi
